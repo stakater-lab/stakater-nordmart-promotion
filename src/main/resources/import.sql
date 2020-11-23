32bd7356819da1d961282cea4517f37b795ad54e
Tero Ahonen <tahonen@redhat.com>
2020-11-23 18:04:22 +0200

    add import data sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO promotion values (1,true,'black-friday-sale-20','Black Friday deal, 20% off', '329199',20);
+INSERT INTO promotion values (2,true,'black-friday-sale-50','Black Friday deal, 50% off', '444434',50);
+INSERT INTO promotion values (3,true,'black-friday-sale-10','Black Friday deal, 10% off', '444435',10);
