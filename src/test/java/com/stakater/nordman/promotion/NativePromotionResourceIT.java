e4b11c3d22c270ce7bebd2a9d0145ac922aa6d05
hanzala <muhammadhanzala12@gmail.com>
2020-11-24 17:42:21 +0500

    removed test files

--- src/test/java/com/stakater/nordman/promotion/NativePromotionResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.stakater.nordman.promotion;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativePromotionResourceIT extends PromotionResourceTest {
-
-    // Execute the same tests but in native mode.
-}
